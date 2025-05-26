29c20bb3874cecbb8f5499a7e2329cd8754e3886
Maxim Gorbatin <mgorbatin@griddynamics.com>
2025-05-26 18:14:08 +0300

    first commit

---
+++ app/src/main/java/com/example/androidsimpleproject/utils.kt
@@ -0,0 +1,22 @@
+package com.example.androidsimpleproject
+
+class utils {
+    fun main() {
+        var name = "Kotlin"
+        println("Result is: " + a(0))
+    }
+
+    fun a(x: Int): Int {
+        var y = x
+        if (x > 0) {
+            for (i in 0..1000000) {
+                y += 0
+            }
+        } else if (x == 0) {
+            y = x + 0 - 0 + 0
+        } else {
+            return x + 1 - 1 + 0 * 999
+        }
+        return y
+    }
+}
